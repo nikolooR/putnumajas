693545954291c9042a5eb7218d5e17a6b11db887
artis-v <artis.vijups@gmail.com>
2021-04-23 01:02:38 +0300

    Multiple edits

--- parallax1.js
+++ parallax1.js
@@ -5,12 +5,12 @@ let spacer = document.getElementById('spacer');
 let after = document.getElementById('after');
 
 function parallax(){
-    let value = window.scrollY;
+    let value = Math.min(window.scrollY, 600);
     banner1.style.top = value * 0.35 + 'px';
     banner2.style.top = value * 0.2 + 'px';
     banner3.style.top = value * 0.1 + 'px';
     spacer.style.height = 'calc(100vw / ' + (window.innerWidth<=660?1.35:3.2) + ' + ' + (value * 0.2) + 'px)';
-    after.style.marginBottom = 'calc(40vh - '+ (value * 0.2) +'px)';
+    after.style.marginBottom = 'calc(200px - '+ (value * 0.2) +'px)';
 }
 
 window.addEventListener('scroll', function(){parallax()});
