eed83c017089a2588b1ab131a90e12bfdbbf4738
artis-v <artis.vijups@gmail.com>
2021-04-22 21:13:59 +0300

    Adjusted banner for mobile

--- parallax1.js
+++ parallax1.js
@@ -4,11 +4,15 @@ let banner3 = document.getElementById('banner3');
 let spacer = document.getElementById('spacer');
 let after = document.getElementById('after');
 
-window.addEventListener('scroll', function(){
+function parallax(){
     let value = window.scrollY;
     banner1.style.top = value * 0.35 + 'px';
     banner2.style.top = value * 0.2 + 'px';
     banner3.style.top = value * 0.1 + 'px';
-    spacer.style.height = 'calc(100vw / 3.2 + ' + (value * 0.2) + 'px)';
+    spacer.style.height = 'calc(100vw / ' + (window.innerWidth<=660?1.35:3.2) + ' + ' + (value * 0.2) + 'px)';
     after.style.marginBottom = 'calc(40vh - '+ (value * 0.2) +'px)';
-});
+}
+
+window.addEventListener('scroll', function(){parallax()});
+window.addEventListener('resize', function(){parallax()});
+window.onload = parallax();
