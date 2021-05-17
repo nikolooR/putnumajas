ddc48df6017089445c8bcfa37d2e39f469c827a7
Nikolajs Rubaniks <nikolajs.rubaniks@inbox.lv>
2021-05-17 14:46:25 +0300

    .
    fuking banner background

--- parallax1.js
+++ parallax1.js
@@ -1,6 +1,11 @@
 let banner1 = document.getElementById('banner1');
 let banner2 = document.getElementById('banner2');
 let banner3 = document.getElementById('banner3');
+
+
+
+
+
 let spacer = document.getElementById('spacer');
 let after = document.getElementById('after');
 
@@ -8,9 +13,14 @@ function parallax(){
     let value = Math.min(window.scrollY, 600);
     banner1.style.top = value * 0.35 + 'px';
     banner2.style.top = value * 0.2 + 'px';
-    banner3.style.top = value * 0.1 + 'px';
+    banner3.style.left = value * 0.1 + 'px';
+
+
+
+   
     spacer.style.height = 'calc(100vw / ' + (window.innerWidth<=660?1.35:3.2) + ' + ' + (value * 0.2) + 'px)';
     after.style.marginBottom = 'calc(200px - '+ (value * 0.2) +'px)';
+ 
 }
 
 window.addEventListener('scroll', function(){parallax()});
